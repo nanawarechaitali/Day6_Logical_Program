0576001381ffa4c022084dc32b37a93ce278c9c4
Your Name <nanawarechaitali4196@gmail.com>
2023-02-06 13:25:41 +0530

    First commit

--- src/com/bridgelabz/utility/Utility.java
+++ src/com/bridgelabz/utility/Utility.java
@@ -3,17 +3,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Utility {
-    public static boolean isAnagramsStings(String str1, String str2){
-        if(str1.length()==str2.length()){
-            char a [] = str1.toCharArray();
+    public static boolean isAnagramsStings(String str1, String str2) {
+        if (str1.length() == str2.length()) {
+            char a[] = str1.toCharArray();
             char b[] = str2.toCharArray();
 
             Arrays.sort(a);
             Arrays.sort(b);
-            if(Arrays.equals(a,b)){
+            if (Arrays.equals(a, b)) {
                 return true;
 
-            }else{
+            } else {
                 return false;
             }
 
@@ -21,39 +21,42 @@ public class Utility {
         return false;
 
     }
-    public static boolean isAnagram(String n1, String n2){
+
+    public static boolean isAnagram(String n1, String n2) {
         char a[] = n1.toCharArray();
-        char b[] =n2.toCharArray();
+        char b[] = n2.toCharArray();
 
 
         Arrays.sort(a);
         Arrays.sort(b);
-        if(Arrays.equals(a,b)){
+        if (Arrays.equals(a, b)) {
             return true;
 
         }
-            return false;
+        return false;
     }
-    public static boolean isPrime(int n){
-        if(n==0||n==1){
+
+    public static boolean isPrime(int n) {
+        if (n == 0 || n == 1) {
             return false;
         }
-            for(int i =2;i<=n;i++){
-                if(n%i==0){
-                    return false;
-                }
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                return false;
             }
-    return true;
+        }
+        return true;
     }
-    public static ArrayList<Integer> isPrimePalindrom(ArrayList<Integer> alist){
-        ArrayList<Integer>blist = new ArrayList<>();
-        for(int i=0; i<alist.size();i++){
+
+    public static ArrayList<Integer> isPrimePalindrom(ArrayList<Integer> alist) {
+        ArrayList<Integer> blist = new ArrayList<>();
+        for (int i = 0; i < alist.size(); i++) {
             int a = alist.get(i);
-            int reverse=0;
-            while(a>0){
-                int r = a%10;
+            int reverse = 0;
+            while (a > 0) {
+                int r = a % 10;
             }
-            if(reverse ==alist.get(i)){
+            if (reverse == alist.get(i)) {
                 blist.add(reverse);
 
             }
